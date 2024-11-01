1ffdf286c85a736167d27ff1e3ce91644cca6d16
Thomas Frank <thomas@franks-im-web.de>
2024-11-01 12:52:51 +0000

    add traefik labels to portainer

--- container.nix
+++ container.nix
@@ -20,6 +20,17 @@ in {
         "/var/run/docker.sock:/var/run/docker.sock"
         "${base_dir}/portainer_data:/data"
       ];
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.portainer.rule" = "(Host(`portainer.bogen-psv.de`))";
+        "traefik.http.routers.portainer.entrypoints" = "websecure";
+        "traefik.http.routers.portainer.tls" = "true";
+        "traefik.http.routers.portainer.tls.certresolver" = "myresolver";
+        "traefik.http.services.portainer.loadbalancer.server.port" = "9000";
+      };
+      extraOptions = [
+        "--network=traefik_proxy"
+      ];
     };
   };
 }
